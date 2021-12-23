1c4d9324db3f209e1de6ea3421ea1e85c2ba16ab
nitslaszlo <nitslaszlo@gmail.com>
2021-12-24 00:59:53 +0100

    add store

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import { createApp } from 'vue';
 
 import vuetify from './plugins/vuetify';
 import routes from './routes';
+import store from './store';
 import App from './app.vue';
 
 const app = createApp(App);
-
+app.config.productionTip = true;
+app.config.devtools = true;
 app.use(routes);
+app.use(store);
 app.use(vuetify);
 
 app.mount('#app');
